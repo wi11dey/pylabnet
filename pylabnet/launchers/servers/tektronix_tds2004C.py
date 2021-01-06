708bbb14b60e2c155ad8d0e6da89c7638d75c7f5
Mihir Bhaskar <mbhaskar@g.harvard.edu>
2021-01-06 15:33:12 -0500

    setup auto launching of scope server

---
+++ pylabnet/launchers/servers/tektronix_tds2004C.py
@@ -0,0 +1,23 @@
+from pylabnet.hardware.oscilloscopes.tektronix_tds2004C import Driver
+from pylabnet.network.client_server.tektronix_tds2004C import Service, Client
+from pylabnet.utils.helper_methods import *
+
+
+def launch(**kwargs):
+
+    logger = kwargs['logger']
+    config_dict = load_device_config(
+        device='tektronix_tds2004C',
+        config=kwargs['config'],
+        logger=logger
+    )
+    scope = Driver(
+        gpib_address=config_dict['device_id'],
+        logger=logger
+    )
+    setup_full_service(
+        service_class=Service,
+        module=scope,
+        logger=logger,
+        host=get_ip()
+    )
